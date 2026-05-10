457dee5d97aca8703c0acd88632f499c76fe8682
Stanisław <stanislawwasilewski2006@gmail.com>
2026-05-10 13:02:42 +0200

    Logic for Rook,Bishop and Queen

---
+++ src/main/java/jchess/model/Bishop.java
@@ -0,0 +1,41 @@
+package jchess.model;
+import java.util.ArrayList;
+import java.util.List;
+public class Bishop extends Piece {
+    private static final int[][] DIRECTIONS = {
+            {-1, -1}, {1, 1}, {1, -1}, {-1, 1}
+    };
+
+    public Bishop(Color color) {
+        super(color);
+    }
+
+    @Override
+    public List<Move> getPossibleMoves(Board board, Square currentSquare) {
+        List<Move> possibleMoves = new ArrayList<>();
+        for(int[] direction: DIRECTIONS){
+            int currentRow = currentSquare.getRow();
+            int currentCol = currentSquare.getCol();
+            while (true) {
+                currentRow += direction[0];
+                currentCol += direction[1];
+                Square newSquare = new Square(currentRow, currentCol);
+
+                if (!newSquare.isValid()) {
+                    break;
+                }
+                Piece targetPiece = board.getPiece(newSquare);
+
+                if (targetPiece == null) {
+                    possibleMoves.add(new Move(currentSquare, newSquare, this, null));
+                } else {
+                    if (targetPiece.getColor() != this.color) {
+                        possibleMoves.add(new Move(currentSquare, newSquare, this, targetPiece));
+                    }
+                    break;
+                }
+            }
+        }
+        return possibleMoves;
+    }
+}
